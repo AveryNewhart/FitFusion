f2b32a4ff1073ab858fe831cd1078bb0cd901004
AveryNewhart <newhartreeceavery@gmail.com>
2023-10-12 18:31:17 -0400

    adding vue apollo

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-
+import Nav from './components/Nav.vue';
+import Home from './components/Home.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
-  <div>
-    <h1 class="underline">FitFusion</h1>
-  </div>
+  <Nav />
+    <div>
+      <Home />
+    </div>
+  <Footer />
 </template>
 
 <style scoped>
 </style>
+
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+  cache,
+  uri: '<https://rickandmortyapi.com/graphql>',
+})
+
+const app = createApp({
+    setup () {
+      provide(DefaultApolloClient, apolloClient)
+    },
+  
+    render: () => h(App),
+  })
+
+  app.mount('#app');
