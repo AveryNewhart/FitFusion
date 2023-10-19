148882db147797aa16af364200d884dee2160cf8
AveryNewhart <newhartreeceavery@gmail.com>
2023-10-18 21:05:34 -0400

    taking out commented out apollo code

--- src/App.vue
+++ src/App.vue
@@ -5,48 +5,12 @@
 </template>
 
 <script lang="ts">
-import { defineComponent
-  // , provide 
-} from 'vue';
-// import { ApolloClient, InMemoryCache,
-//   //  createHttpLink 
-//   } from '@apollo/client/core';
-// import { setContext } from '@apollo/client/link/context';
 
-// import { DefaultApolloClient } from '@vue/apollo-composable';
-
-
-
-// Construct our main GraphQL API endpoint
-// const httpLink = createHttpLink({
-//   uri: '/graphql',
-// });
-
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
-// const authLink = setContext((_, { headers }) => {
-//   // get the authentication token from local storage if it exists
-//   const token = localStorage.getItem('id_token');
-//   // return the headers to the context so httpLink can read them
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
-
-// const apolloClient = new ApolloClient({
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
+import { defineComponent } from 'vue';
 
 
 export default defineComponent({
   setup() {
-    // Provide the Apollo Client using DefaultApolloClient
-    // provide(DefaultApolloClient, 
-    // // apolloClient
-    // );
 
   },
 });
