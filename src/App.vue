5be15015e92f1d8a0c517e1aea0c910f279ef3ea
AveryNewhart <newhartreeceavery@gmail.com>
2023-10-18 21:02:46 -0400

    routes created for home and proflile page, figured them out

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
-<script setup lang="ts">
-import Nav from './components/Nav.vue';
-import Home from './components/Home.vue';
-import Footer from './components/Footer.vue';
-</script>
-
 <template>
-  <Nav />
-    <div>
-      <Home />
-    </div>
-  <Footer />
+  <div>
+    <router-view></router-view>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<script lang="ts">
+import { defineComponent
+  // , provide 
+} from 'vue';
+// import { ApolloClient, InMemoryCache,
+//   //  createHttpLink 
+//   } from '@apollo/client/core';
+// import { setContext } from '@apollo/client/link/context';
+
+// import { DefaultApolloClient } from '@vue/apollo-composable';
+
+
+
+// Construct our main GraphQL API endpoint
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem('id_token');
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+
+// const apolloClient = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
+
+
+export default defineComponent({
+  setup() {
+    // Provide the Apollo Client using DefaultApolloClient
+    // provide(DefaultApolloClient, 
+    // // apolloClient
+    // );
+
+  },
+});
+</script>
+
+
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
+
+
+
 </script>
 
 <template>
 
-  <div class="">
+  <div class="theHome">
     <h1>home</h1>
+    <p>this is the home page</p>
   </div>
 
 </template>
 
 <style scoped>
+
+.theHome {
+  color: black;
+}
+
 </style>
