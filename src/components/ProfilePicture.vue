b94130c774d48e8e88a3323493e889df37f0f90f
AveryNewhart <newhartreeceavery@gmail.com>
2023-11-01 19:30:58 -0400

    user can add a profile picture while on their proflie, obvisouly later will have to set routes for when user views another users profile

--- src/components/ProfilePicture.vue
+++ src/components/ProfilePicture.vue
@@ -1,16 +1,45 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from 'vue';
 
+export default defineComponent({
+  data() {
+    return {
+      imageUrl: '',
+    };
+  },
+  methods: {
+    handleFileChange(e: Event) {
+      const input = e.target as HTMLInputElement;
+      if (input.files && input.files[0]) {
+        this.imageUrl = URL.createObjectURL(input.files[0]);
+      }
+    },
+  },
+});
 </script>
 
 <template>
-<div>
-  <Nav />
-</div>
-  <div class="">
-    <h1>This is where profile picture logic will be.</h1>
-  </div>
-
-</template>
+    <div class="profilePictureDiv">
+      <h2>Upload Input</h2>
+      <img :src="imageUrl" alt="" class="uploaded-image">
+      <form action="index.html" method="post">
+        <input type="file" name="file" id="file" @change="handleFileChange" accept="image/*">
+        <!-- <label for="file">Select a file ...</label> -->
+      </form>
+    </div>
+  </template>
 
 <style scoped>
+.uploaded-image {
+  max-width: 100px;
+  height: 100px; 
+  border-radius: 50px;
+}
+
+.profilePictureDiv {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid red;
+}
+
 </style>
