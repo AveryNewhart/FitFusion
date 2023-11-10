e2eb484076956f350dfbc3291a901d9a252cd1c6
AveryNewhart <newhartreeceavery@gmail.com>
2023-11-10 15:24:30 -0500

    starting to get the general layout for the running routes page

--- src/components/RunningRoutes.vue
+++ src/components/RunningRoutes.vue
@@ -2,50 +2,58 @@
 
 import Nav from './/Nav.vue';
 
-// THIS IS COMMENTED OUT CODE FOR MAPBOX API. API THAT MIGHT BE USED.
-// ANOTHER ONE TO LOOK INTO, IS SMASHAPI, IT HAS A FEW THINGS I WOULD LIKE TO USE 
-
-// import { ref, onMounted } from 'vue';
-// import type { Map } from 'mapbox-gl';
-
-
-// mapbox-gl.accessToken = 'pk.eyJ1IjoiYXZlcnluZXdoYXJ0IiwiYSI6ImNsb2MzZWhldTBobGgyam80cWVqNjRraHQifQ.y2LJ6rP0_ze_zP3yXaSvTQ';
-
-// const map = ref<Map | null>(null);
-
-// const toggleForm = () => {
-
-// };
-
-// onMounted(() => {
-//   const mapContainer = document.getElementById('map-container');
-//   if (mapContainer) {
-//     const mapInstance = new mapboxgl.Map({
-//       container: mapContainer,
-//       style: 'mapbox://styles/mapbox/streets-v12',
-//       center: [-71.224518, 42.213995],
-//       zoom: 9,
-//     });
-//     map.value = mapInstance;
-//   }
-// });
 
 </script>
 
-<!-- HAVE IT SO THERE IS A MAP WHERE A USER CAN SEE RUNNING ROUTES IN THE AREA -->
-<!-- HAVE IT SO THE USER CAN "CREATE A ROUTE" THEN THE ROUTE SAVES FROM THEIR RUN, THEN THE ROUTE IS SAVED TO THE USERS SAVED ROUTES SECTION ON THE PAGE -->
-<!-- HAVE AN AREA APPEAR WHEN A USER SELECTS A ROUTE, THE TOP 3 TIMES RECORDED ON THE ROUTE WILL SHOW, AND BY WHO -->
+<!-- I WANT TO HAVE A SEARCH BAR FOR THE USER TO SEARCH A LOCATION AND A BUTTON WITH THAT THAT THE USER CLICKS WHEN THEY WANT TO SEARCH THE LOCATION(LOGIC WILL BE ADDED LATER WHEN API FOR MAP IS ADDED) -->
+<!-- I WANT TO INCLUDE A PERSONAL BEST SECTION FOR THE USER NO MATTER THEIR TIME, ON ANY ROUTE WHEN THE USER CLICKS ON IT, IT DOESN'T HAVE TO BE A SAVED ROUTE EITHER. SO A SECTION FOR THAT NEEDS TO BE ADDED -->
+
+
 
 <template>
-<div>
-  <Nav />
-</div>
-  <div class="">
-    <h1>generate running routes</h1>
-    <div ref="mapContainer" class="map-container"></div>
+  <div>
+    <Nav />
   </div>
+  <div class="flex flex-col items-center mt-8">
+    <h1 class="text-3xl font-semibold mb-4">Generate Running Routes</h1>
 
+    <!-- MAP CONTAINER -->
+    <div ref="mapContainer" class="w-full h-96 rounded-lg mb-8 mapContainer">the map will be in here</div>
+    <!-- MAP CONTAINER -->
+
+    <!-- save a route -->
+    <div class="mb-4">
+      <button class="bg-blue-500 text-white px-4 py-2 rounded">Save This Route</button>
+    </div>
+
+    <!-- saved routes section -->
+    <div class="w-full">
+      <h2 class="text-xl font-semibold mb-2">Your Saved Routes</h2>
+      <!-- list of user's saved routes here -->
+      <!-- the routes will be clickable links that once clicked, the map will pull up that route. -->
+      <ul class="list-disc pl-4">
+        <a href="runningroutes"><li>Route 1</li></a>
+        <a href="runningroutes"><li>Route 2</li></a>
+      </ul>
+    </div>
+
+    <!-- selected route section -->
+    <div class="w-full mt-8">
+      <h2 class="text-xl font-semibold mb-2">Top Times On This Route</h2>
+      <!-- top 3 times recorded -->
+      <ol class="list-decimal pl-4">
+        <li>Paul - 20:30</li>
+        <li>Avery - 22:15</li>
+        <li>Fred n Tiny (duo) - 23:00</li>
+      </ol>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+
+.mapContainer {
+  border: 2px solid darkgreen;
+}
+
 </style>
