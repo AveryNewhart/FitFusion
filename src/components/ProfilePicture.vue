c93c7922cc293c2948617f2ca2988b6f13ea16a8
AveryNewhart <newhartreeceavery@gmail.com>
2023-11-17 18:36:07 -0500

    cleaned up profile picture code, now just need to adjust code or since the profile picture will save to the user, just not include the profile picture logic on another users profile

--- src/components/ProfilePicture.vue
+++ src/components/ProfilePicture.vue
@@ -5,6 +5,7 @@ export default defineComponent({
   data() {
     return {
       imageUrl: '',
+      showFileInput: false,
     };
   },
   methods: {
@@ -12,32 +13,41 @@ export default defineComponent({
       const input = e.target as HTMLInputElement;
       if (input.files && input.files[0]) {
         this.imageUrl = URL.createObjectURL(input.files[0]);
+        this.showFileInput = false; // Hide the file input after selecting a file
       }
+    },
+    showEditOptions() {
+      this.showFileInput = true;
     },
   },
 });
 </script>
 
-<!-- I WANT THE PROFILE PICTURE TO BE DISPLAYED WITH A TINY EDIT BUTTON NEXT TO IT. -->
-<!-- ONCE THE EDIT BUTTON IS CLICKED THEN THE FILES POP UO TO SELECT A NEW PHOTO. -->
 <!-- IT ISN'T CLEAN CURRENTLY, AND WILL ALSO ONLY WORK FOR ON THE USERS PROFILE, BECAUSE I DONT WANT TO HAVE ANY USER BE ABLE TO CHANGE ANY USERS PROFILE PICTURE.  -->
 
 <template>
+  <div>
     <div>
-      <h2>Upload Profile Picture</h2>
       <img :src="imageUrl" alt="" class="uploaded-image">
-      <form action="index.html" method="post">
-        <input type="file" name="file" id="file" @change="handleFileChange" accept="image/*">
-        <!-- <label for="file">Select a file ...</label> -->
-      </form>
+      <button class="editBtn" @click="showEditOptions">Edit</button>
     </div>
-  </template>
+    <form v-if="showFileInput" action="index.html" method="post">
+      <input type="file" name="file" id="file" @change="handleFileChange" accept="image/*">
+    </form>
+  </div>
+</template>
 
 <style scoped>
 .uploaded-image {
   max-width: 100px;
   height: 100px; 
   border-radius: 50px;
+}
+
+.editBtn {
+  background-color: gray;
+  color: aquamarine;
+  border: 1px solid green;
 }
 
 
