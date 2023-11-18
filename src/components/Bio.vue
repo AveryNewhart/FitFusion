55c8fbbb97720fac5320eb65a08f173a4808fce4
AveryNewhart <newhartreeceavery@gmail.com>
2023-11-18 17:36:43 -0500

    made the bio editable and a modal appear when the user clicks edit, save and exit buttons made as well

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -1,17 +1,42 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const bio = ref<string>('');
+const newBio = ref<string>('');
+const isModalOpen = ref<boolean>(false);
+
+const openModal = () => {
+  isModalOpen.value = true;
+  newBio.value = bio.value; // makes the modal text, the users entered text
+};
+
+const closeModal = () => {
+  isModalOpen.value = false;
+};
+
+const saveBio = () => {
+  bio.value = newBio.value;
+  isModalOpen.value = false;
+};
 </script>
 
-<!-- I WANT THE BIO TO DISPLAY A FIELD OF TEXT THAT THE USER INPUTS, SO IT WILL BE BLANK OF COURSE WHEN NOTHING IS IN THERE. -->
-<!-- THERE SHOULD BE A SMALL EDIT BUTTON THE USER SHOULD BE ABLE TO CLICK AND WHEN THEY CLICK IT, A MODAL POPS UP WITH A TEXT FIELD THAT ALLOWS THE USER TO ENTER THEIR BIO. -->
-<!-- THE MODAL SHOULD HAVE A SAVE BUTTON THAT CLOSES THE MODAL AND SAVES THE USERS TEXT AND DISPLAYS IT IN THE BIO SECTION -->
-<!-- THERE SHOULD ALSO JUST BE A "X" WITH A RED BACKGROUND TO CLOSE THE MODAL THAT DOESNT SAVE/UPDATE THE BIO OF THE USER. -->
 
 <template>
+  <div class="relative">
+    <h3>{{ bio || 'Bio is empty' }}</h3>
+    <button @click="openModal" class="ml-2 text-blue-500 cursor-pointer">Edit</button>
 
-  <div class="">
-    <h3>bio is here</h3>
+    <!-- modal -->
+    <div v-if="isModalOpen" class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+      <div class="bg-white p-4 rounded-lg">
+        <div class="flex justify-end">
+          <button @click="closeModal" class="text-red-500">&times;</button>
+        </div>
+        <textarea v-model="newBio" class="w-full h-32 p-2 border rounded mb-4"></textarea>
+        <button @click="saveBio" class="bg-blue-500 text-white px-4 py-2 rounded">Save</button>
+      </div>
+    </div>
   </div>
-
 </template>
 
 <style scoped>
