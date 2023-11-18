39ae05c5588868a0714002dc5425df3e9a090b8b
AveryNewhart <newhartreeceavery@gmail.com>
2023-11-18 16:43:43 -0500

    followers list component, added total number to show, and added a modal with a search bar

--- src/components/Followers.vue
+++ src/components/Followers.vue
@@ -1,17 +1,64 @@
 <script setup lang="ts">
+import { ref, computed } from 'vue';
+
+// replace this with your actual data
+const followers = [
+  { id: 1, name: 'Follower 1' },
+  { id: 2, name: 'Follower 2' },
+  { id: 3, name: 'Follower 3' },
+  // add more followers as needed to see modal is scrollable
+];
+
+const totalFollowers = ref(followers.length);
+const isModalVisible = ref(false);
+const searchQuery = ref('');
+
+const filteredFollowers = computed(() => {
+  return followers.filter((follower) =>
+    follower.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+  );
+});
+
+const toggleFollowerList = () => {
+  isModalVisible.value = !isModalVisible.value;
+};
 </script>
 
-<!-- HAVE A NUMBER NEXT TO THE FOLLOWER LABEL OF THE TOTAL USERS THAT ARE FOLLOWING THE USER. -->
-
-<!-- HAVE IT BE A LINK THAT OPENS A MODAL OR DISPLAYS A NICE LIST OF ALL THE USERS THAT ARE FOLLOWING THE USER WITH A SEARCH BAR THAT IS ABLE TO SEARCH THE LIST -->
 
 <template>
-
   <div class="">
     <p>Follower List</p>
-  </div>
 
+    <!-- display the number of followers -->
+    <p>Total Followers: {{ totalFollowers }}</p>
+
+    <!-- button to open/close the modal -->
+    <button class="viewFollowersBtn" @click="toggleFollowerList">
+      {{ isModalVisible ? 'Close Followers' : 'View Followers' }}
+    </button>
+
+    <!-- modal to display followers -->
+    <div v-if="isModalVisible">
+      <!-- search bar -->
+      <input v-model="searchQuery" placeholder="Search followers">
+
+      <!-- list of followers -->
+      <ul>
+        <li v-for="follower in filteredFollowers" :key="follower.id">
+          {{ follower.name }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+
+.viewFollowersBtn {
+  background-color: purple;
+  border: 1px solid limegreen;
+  margin: 5px;
+  cursor: pointer;
+}
+
 </style>
