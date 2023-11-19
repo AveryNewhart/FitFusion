9e0e704a1455fedf4057f9cf781a0c5f3a909d71
AveryNewhart <newhartreeceavery@gmail.com>
2023-11-19 14:28:54 -0500

    added the color scheme to the login/signup page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -14,6 +14,8 @@ const toggleForm = () => {
 
 </script>
 
+<!-- HAVE A HIDE/SHOW BUTTON TO SEE THE LETTERS OF THE PASSWORD -->
+
 <template>
     <div>
       <Nav />
@@ -21,41 +23,41 @@ const toggleForm = () => {
         <!-- Login Form -->
         <transition name="flip" mode="out-in">
             <div :key="loginMode" class="login-box login-box-1" v-if="loginMode === 'login'">
-                <h2 class="text-3xl font-semibold text-center text-gray-800 mb-6">Login</h2>
+                <h2 class="text-3xl font-semibold text-center text-white mb-6">Login</h2>
                 <form id="login-form" class="space-y-4">
                     <div class="relative">
-                        <input type="text" name="username-login" id="username-login" placeholder=" " class="peer h-10 w-full border-b-2 border-gray-300 focus:outline-none focus:border-blue-500">
-                        <label for="username-login" class="absolute top-0 -z-1 duration-300 origin-0 transform translate-y-2 text-gray-600">Username</label>
+                        <input type="text" name="username-login" id="username-login" placeholder="" class="peer h-10 w-full border-b-2 focus:outline-none inputColor">
+                        <label for="username-login" class="absolute top-0 -z-1 duration-300 origin-0 transform translate-y-2 textColor">Username</label>
                     </div>
                     <div class="relative">
-                        <input type="password" name="password-login" id="password-login" placeholder=" " class="peer h-10 w-full border-b-2 border-gray-300 focus:outline-none focus:border-blue-500">
-                        <label for="password-login" class="absolute top-0 -z-1 duration-300 origin-0 transform translate-y-2 text-gray-600">Password</label>
+                        <input type="password" name="password-login" id="password-login" placeholder=" " class="peer h-10 w-full border-b-2 focus:outline-none inputColor">
+                        <label for="password-login" class="absolute top-0 -z-1 duration-300 origin-0 transform translate-y-2 textColor">Password</label>
                     </div>
-                    <button class="login-btn relative overflow-hidden text-white bg-gradient-to-r from-green-400 to-blue-500 py-2 px-4 rounded-full transform transition hover:scale-105">
-                        <span class="absolute top-0 left-0 w-full h-full transform scale-x-0 bg-gradient-to-r from-green-400 to-blue-500"></span>
+                    <button class="login-btn relative overflow-hidden text-white py-2 px-4 rounded-full transform transition hover:scale-105 buttonColor">
+                        <span class="absolute top-0 left-0 w-full h-full transform scale-x-0"></span>
                         Submit
                     </button>
                 </form>
-                <button class="toggle-form text-blue-500 hover:underline" @click="toggleForm">No account, sign up here</button>
+                <button class="toggle-form switchColor" @click="toggleForm">No account, sign up here</button>
             </div>
           <!-- Signup Form -->
             <div :key="loginMode + '-signup'" class="login-box login-box-2" v-else>
-                <h2 class="text-3xl font-semibold text-center text-gray-800 mb-6">Sign-Up</h2>
+                <h2 class="text-3xl font-semibold text-center text-white mb-6">Sign-Up</h2>
                 <form id="signup-form" class="space-y-4">
                     <div class="relative">
-                        <input type="text" name="username-signup" id="username-signup" placeholder=" " class="peer h-10 w-full border-b-2 border-gray-300 focus:outline-none focus:border-blue-500">
-                        <label for="username-signup" class="absolute top-0 -z-1 duration-300 origin-0 transform translate-y-2 text-gray-600">Username</label>
+                        <input type="text" name="username-signup" id="username-signup" placeholder=" " class="peer h-10 w-full border-b-2 focus:outline-none inputColor">
+                        <label for="username-signup" class="absolute top-0 -z-1 duration-300 origin-0 transform translate-y-2 textColor">Username</label>
                     </div>
                     <div class="relative">
-                        <input type="password" name="password-signup" id="password-signup" placeholder=" " class="peer h-10 w-full border-b-2 border-gray-300 focus:outline-none focus:border-blue-500">
-                        <label for="password-signup" class="absolute top-0 -z-1 duration-300 origin-0 transform translate-y-2 text-gray-600">Password</label>
+                        <input type="password" name="password-signup" id="password-signup" placeholder=" " class="peer h-10 w-full border-b-2 focus:outline-none inputColor">
+                        <label for="password-signup" class="absolute top-0 -z-1 duration-300 origin-0 transform translate-y-2 textColor">Password</label>
                     </div>
-                    <button class="login-btn relative overflow-hidden text-white bg-gradient-to-r from-green-400 to-blue-500 py-2 px-4 rounded-full transform transition hover:scale-105">
-                        <span class="absolute top-0 left-0 w-full h-full transform scale-x-0 bg-gradient-to-r from-green-400 to-blue-500"></span>
+                    <button class="login-btn relative overflow-hidden text-white py-2 px-4 rounded-full transform transition hover:scale-105 buttonColor">
+                        <span class="absolute top-0 left-0 w-full h-full transform scale-x-0"></span>
                         Submit
                     </button>
                 </form>
-                <button class="toggle-form text-blue-500 hover:underline" @click="toggleForm">Already have an account? Log in</button>
+                <button class="toggle-form switchColor" @click="toggleForm">Already have an account? Log in</button>
             </div>
         </transition>
       </div>
@@ -71,6 +73,45 @@ const toggleForm = () => {
   align-items: center;
   justify-content: center;
   height: 100vh;
+  background-color: #2d2d2d;
+}
+
+.textColor {
+  /* color: #925ff0; */
+  color: white;
+  margin-left: 5px;
+}
+
+.inputColor {
+  background-color: #a3fda1;
+  border: 1px solid #925ff0;
+  border-radius: 5px;
+  color: #2d2d2d;
+}
+
+.buttonColor {
+  background-color: #925ff0;
+  color: #a3fda1;
+  font-weight: bold;
+  border: 1px solid #a3fda1;
+}
+
+.buttonColor:hover {
+  background-color: #a3fda1;
+  color: #925ff0;
+  border: 1px solid #925ff0;
+}
+
+.switchColor {
+  color: #925ff0;
+  border-bottom: 1px solid #a3fda1;
+  margin-top: 10px;
+}
+
+.switchColor:hover {
+  background-color: #a3fda1;
+  border-radius: 10px;
+  padding: 2px;
 }
 
 
