cb65e05a11612a08698f614337ed33b6fd3494cb
AveryNewhart <newhartreeceavery@gmail.com>
2023-11-29 17:56:54 -0500

    put the working searchbar from mapbox above the map

--- src/views/RunningRoutes.vue
+++ src/views/RunningRoutes.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {  onMounted, onBeforeUnmount } from 'vue';
+import {  ref, onMounted, onBeforeUnmount } from 'vue';
 import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
@@ -9,6 +9,8 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import Nav from '../components/Nav.vue';
 
 let map: mapboxgl.Map | null = null;
+let geocoder: MapboxGeocoder | null = null;
+const searchLocation = ref('');
 
   onMounted(() => {
     // initialize the map
@@ -26,21 +28,16 @@ let map: mapboxgl.Map | null = null;
       map.addControl(new mapboxgl.GeolocateControl({ positionOptions: { enableHighAccuracy: true }, trackUserLocation: true }), 'top-left');
       map.addControl(new mapboxgl.ScaleControl(), 'bottom-left');
       map.addControl(new mapboxgl.FullscreenControl(), 'top-left');
-      map.addControl(new mapboxgl.AttributionControl(), 'bottom-right');
-
-      const attributionControl = document.querySelector('.mapboxgl-ctrl-attrib');
-        if (attributionControl) {
-          (attributionControl as HTMLElement).style.display = 'none';
-        }
 
 
-      // geocoder control to the map
-      const geocoder = new MapboxGeocoder({
-        accessToken: 'pk.eyJ1IjoiYXZlcnluZXdoYXJ0IiwiYSI6ImNsb2MzZWhldTBobGgyam80cWVqNjRraHQifQ.y2LJ6rP0_ze_zP3yXaSvTQ',
-        mapboxgl: mapboxgl,
-      });
+      // // geocoder control to the map
+      // const geocoder = new MapboxGeocoder({
+      //   accessToken: 'pk.eyJ1IjoiYXZlcnluZXdoYXJ0IiwiYSI6ImNsb2MzZWhldTBobGgyam80cWVqNjRraHQifQ.y2LJ6rP0_ze_zP3yXaSvTQ',
+      //   mapboxgl: mapboxgl,
+      // });
+      
 
-      map.addControl(geocoder);
+      // map.addControl(geocoder);
 
       // draw control to the map
       const draw = new MapboxDraw({
@@ -49,11 +46,37 @@ let map: mapboxgl.Map | null = null;
           line_string: true,
           trash: true,
         },
+        styles: [
+          {
+            id: 'gl-draw-line',
+            type: 'line', // mess with this area to get the line to be dashed while drawing, then solid when then user is done drawing the route. 
+            paint: {
+              'line-color': '#925ff0', // line color
+              'line-width': 2,
+            },
+          },
+          ],
       });
 
       map.addControl(draw);
 
-    // any additional map functionality here
+        //  Mapbox Geocoder control
+      geocoder = new MapboxGeocoder({
+        accessToken: 'pk.eyJ1IjoiYXZlcnluZXdoYXJ0IiwiYSI6ImNsb2MzZWhldTBobGgyam80cWVqNjRraHQifQ.y2LJ6rP0_ze_zP3yXaSvTQ',
+        mapboxgl: mapboxgl,
+      });
+
+      // Geocoder control to the integrated search bar
+      geocoder.addTo('.mySearch');
+
+      // handle geocoder result
+      geocoder.on('result', (result) => {
+      // log the result to the console for now
+      console.log('Geocoder result:', result);
+
+      // center the map on the result
+      map!.flyTo({ center: result.result.center, zoom: 12 });
+    });
   });
 
   onBeforeUnmount(() => {
@@ -62,16 +85,45 @@ let map: mapboxgl.Map | null = null;
   }
 });
 
+// Trigger Mapbox Geocoder search when custom search button is clicked
+// const searchRoutes = () => {
+//   // use the value of searchLocation here
+//   // if (searchLocation.value && geocoder) {
+//   //   // Manually set the Geocoder input value
+//   //   geocoder.setInput(searchLocation.value);
 
+//   //   // Trigger the Geocoder search
+//   //   geocoder.on('result', (result) => {
+//   //     console.log('Geocoder result:', result);
+//   //     // Handle the result here, e.g., center the map on the result
+//   //     map?.flyTo({ center: result.result.center, zoom: 12 });
+//   //   });
+//   // }
+// };
+
+
+// Trigger Mapbox Geocoder search when custom search button is clicked
+const searchRoutes = () => {
+  if (searchLocation.value && geocoder) {
+    // Manually set the Geocoder input value
+    geocoder.setInput(searchLocation.value);
+
+    // Trigger the Geocoder search
+    geocoder.query(searchLocation.value);
+  }
+};
 
 
 </script>
 
-<!-- FINISH ADDING COLOR SCHEME -->
+<!-- GET SEARCH BAR TO WORK FOR MY CUSTOM SEARCH BAR, FIGURE OUT HOW TO STYLE SEARCH BAR NOW, OR SEE I IF OTHER SEARCH BAR ON MAPBOX SITE -->
+<!-- DO I HAVE TO BUT THE SEARCH ROUTE LOGIC INTO THE MAP FUNCTION? LIKE WHERE GEOCODER IS DEFINED, DO I PUT THAT INSIDE THE SEARCH ROUTES FUNCTION? -->
+
+<!-- GET ROUTE RUNNING LINES TO BE DOTTED WHILE DRAWING THEN SOLID WHEN FINISHED -->
+
+<!-- ICONS FOR DRAWING BUTTONS -->
 
 <!-- MAKE IT SO MAP FULLSCREEN WORKS, CHANGE THE HEIGHT TO BE ABLE TO BECOME THE MAX HEIGHT -->
-
-<!-- STYLE THE MAP -->
 
 <!-- when mobile styling the running data boxes, adjust the height size of the text to sync up with the size of the boxes downsizing for when users have zoom feature on their phone -->
 
@@ -83,6 +135,20 @@ let map: mapboxgl.Map | null = null;
   <div class="mainRunningDiv min-h-screen">
     <div class="flex flex-col items-center">
       <h1 class="text-3xl font-semibold mb-4 runningHeader">Running Routes</h1>
+
+      <div class="mb-4 mySearch">
+        <input
+          v-model="searchLocation"
+          placeholder="where's the next run?"
+          class="border rounded px-3 py-2"
+        />
+        <button
+          @click="searchRoutes"
+          class="saveBtn px-4 py-2 ml-2"
+        >
+          Search
+        </button>
+      </div>
 
       <!-- MAP CONTAINER -->
       <div ref="map" id="map" class="w-full h-96 rounded-lg mb-8 map"></div>
@@ -132,7 +198,7 @@ let map: mapboxgl.Map | null = null;
 <style scoped>
 .map {
   border: 2px solid darkgreen;
-  z-index: 999;
+  /* z-index: 999; */
 }
 
 .mainRunningDiv {
@@ -181,5 +247,8 @@ let map: mapboxgl.Map | null = null;
   border: 3px solid #925ff0;
   color: #2d2d2d;
 }
+
+
+
 
 </style>
