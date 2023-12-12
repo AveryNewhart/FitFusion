2004dc7b1b3f94c8d713979c98356c1e355b7ef8
AveryNewhart <newhartreeceavery@gmail.com>
2023-12-12 14:13:55 -0500

    made the login page the home page, removed original homepage

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import CalorieTracker from '../views/CalorieTracker.vue'
 import Feed from '../views/Feed.vue'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Profile from '../views/Profile.vue'
 import RunningRoutes from '../views/RunningRoutes.vue'
 import WorkoutCalender from '../views/WorkoutCalender.vue'
 
 const routes = [
-  { 
-    path: '/', 
-    name: 'Home', 
-    component: Home 
-  },
+  // { 
+  //   path: '/', 
+  //   name: 'Home', 
+  //   component: Home 
+  // },
   { 
     path: '/profile', 
     name: 'Profile', 
     component: Profile 
   },
   { 
-    path: '/login', 
+    path: '/', 
     name: 'Login', 
     component: Login 
   },
